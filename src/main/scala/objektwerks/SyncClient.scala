f81a314f35ffc9522242b028442ff39a8f275894
objektwerks <objektwerks@runbox.com>
2023-08-22 12:52:34 -0400

    refactored sync client

--- src/main/scala/objektwerks/SyncClient.scala
+++ src/main/scala/objektwerks/SyncClient.scala
@@ -2,7 +2,7 @@ package objektwerks
 
 import sttp.client3.{SimpleHttpClient, UriContext, basicRequest}
 
-object SyncClient:
+@main def runSyncClient(): Unit =
   val client = SimpleHttpClient()
   val response = client.send(basicRequest.get(uri"https://api.chucknorris.io/jokes/random"))
   println(response.body)
