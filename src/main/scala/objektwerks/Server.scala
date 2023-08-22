e6097c6b8662d9e385bcb4967ea9b8772411dd3d
objektwerks <objektwerks@runbox.com>
2023-08-22 10:53:21 -0400

    added server

---
+++ src/main/scala/objektwerks/Server.scala
@@ -0,0 +1,40 @@
+package objektwerks
+
+import com.sun.net.httpserver.HttpServer
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+
+import java.net.InetSocketAddress
+import java.util.concurrent.Executors
+
+object Server extends LazyLogging:
+  private val config = ConfigFactory.load("server.conf")
+  private val host = config.getString("host")
+  private val port = config.getInt("port")
+  private val address = InetSocketAddress(port)
+  private val backlog = 0
+  private val path = "/now"
+  private val handler = NowHandler()
+  private val filter = CorsFilter()
+  private val http = HttpServer
+    .create(
+      address,
+      backlog,
+      path,
+      handler,
+      filter
+    )
+
+  @main def main(): Unit =
+    http.setExecutor( Executors.newVirtualThreadPerTaskExecutor() )
+    http.start()
+
+    println(s"*** Press Control-C to shutdown server at: $host:$port")
+    logger.info(s"*** Http Server started at: $host:$port")
+
+    sys.addShutdownHook {
+      logger.info(s"*** Http Server shutdown at: $host:$port")
+      http.stop(3)
+    }
+
+    Thread.currentThread().join()
