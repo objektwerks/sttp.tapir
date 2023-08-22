4c89b4a34d95267edfaa3334419278a124231b77
objektwerks <objektwerks@runbox.com>
2023-08-22 13:19:22 -0400

    refactored sync client

--- src/main/scala/objektwerks/SyncClient.scala
+++ src/main/scala/objektwerks/SyncClient.scala
@@ -4,6 +4,13 @@ import sttp.client3.{SimpleHttpClient, UriContext, basicRequest}
 
 @main def runSyncClient(): Unit =
   val client = SimpleHttpClient()
-  val response = client.send(basicRequest.get(uri"https://api.chucknorris.io/jokes/random"))
-  println(response.body)
+  val response = client.send( basicRequest.get(uri"https://api.chucknorris.io/jokes/random") )
+  println( parseResponse( response.body ) )
   client.close()
+
+def parseResponse(response: Either[String, String]): String =
+  response match
+    case Left(error) => s"Sync Client error: $error"
+    case Right(json) => s"Sync Client response: ${parseJson(json)}"
+
+def parseJson(json: String): String = ujson.read(json)("value").str
