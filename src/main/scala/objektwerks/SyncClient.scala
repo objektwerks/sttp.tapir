a8df82af7cb393cce47688df92e6c207ae20ee35
objektwerks <objektwerks@runbox.com>
2024-08-26 10:15:35 -0400

    renamed sttp sync client to sync client

--- src/main/scala/objektwerks/SyncClient.scala
+++ src/main/scala/objektwerks/SyncClient.scala
@@ -3,7 +3,7 @@ package objektwerks
 import sttp.client3.{basicRequest, SimpleHttpClient, UriContext}
 import sttp.client3.logging.slf4j.Slf4jLoggingBackend
 
-@main def runSttpSyncClient(): Unit =
+@main def runSyncClient(): Unit =
   val httpClient = SimpleHttpClient().wrapBackend(Slf4jLoggingBackend(_))
 
   try
