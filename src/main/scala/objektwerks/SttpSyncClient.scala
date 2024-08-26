4e9e7a816be6dca0cf4cdd317d127723dd0c3716
objektwerks <objektwerks@runbox.com>
2024-08-26 10:04:00 -0400

    edited sttp sync client

--- src/main/scala/objektwerks/SttpSyncClient.scala
+++ src/main/scala/objektwerks/SttpSyncClient.scala
@@ -4,13 +4,13 @@ import sttp.client3.{basicRequest, SimpleHttpClient, UriContext}
 import sttp.client3.logging.slf4j.Slf4jLoggingBackend
 
 @main def runSttpSyncClient(): Unit =
-  val client = SimpleHttpClient().wrapBackend(Slf4jLoggingBackend(_))
+  val httpClient = SimpleHttpClient().wrapBackend(Slf4jLoggingBackend(_))
 
   try
     val request = basicRequest.get(uri"https://api.chucknorris.io/jokes/random")
-    val response = client.send(request)
+    val response = httpClient.send(request)
     println( parseResponse(response.body) )
-  finally client.close()
+  finally httpClient.close()
 
   def parseResponse(response: Either[String, String]): String =
     response match
