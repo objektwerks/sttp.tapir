63e6666fbff0a1129028c7da445ba6c364452788
objektwerks <objektwerks@runbox.com>
2024-08-26 11:14:37 -0400

    edited imports

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -3,6 +3,6 @@ package objektwerks
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
-import sttp.tapir.*
+import sttp.tapir.Schema
 
 final case class Event(name: String) derives ConfiguredJsonValueCodec, Schema
