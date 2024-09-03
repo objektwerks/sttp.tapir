de90f9d8f7bf43b62cc1e4606ca14fa5dae18a5f
objektwerks <objektwerks@runbox.com>
2024-09-03 14:09:44 -0400

    added command type enum

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -3,7 +3,10 @@ package objektwerks
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
-final case class Command(name: String)
+enum CommandType:
+  case test
+
+final case class Command(name: String, typeof: CommandType = CommandType.test)
 
 object Command:
   given JsonValueCodec[Command] = JsonCodecMaker.make[Command](CodecMakerConfig.withDiscriminatorFieldName(None))
--- src/main/scala/objektwerks/Schemas.scala
+++ src/main/scala/objektwerks/Schemas.scala
@@ -3,5 +3,7 @@ package objektwerks
 import sttp.tapir.*
 
 object Schemas:
+  given Schema[CommandType] = Schema.derived
   given Schema[Command] = Schema.derived
+  
   given Schema[Event] = Schema.derived
